6abe0f12b4ac813fb475e836f2ec17fa2e88c95a
root <root@ip-172-31-79-47.ec2.internal>
2024-09-09 07:37:42 +0000

    react

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-echo "Starting deployment..."
-
-# Navigate to the destination folder
-cd /var/www/html/frontend-only
-
-# Remove existing files
-rm -rf *
-
-# Move new build files to the folder
-cp -r /* /var/www/html/frontend-only/
-
-echo "Deployment complete."
+cd /home/ubuntu/frontend
+npm install
+npm run build
