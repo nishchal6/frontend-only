fe0602b1d19036fccc12599e460f5421106489f1
Nishchal garg <nishchalgarg08@gmail.com>
2024-09-09 16:21:39 +0530

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/html/strapi.fur4.com
-pm2 delete strapi-app || true
-pm2 start npm --name "strapi-app" -- run develop
+pm2 delete app || true
+pm2 start npm --name "app" -- run develop
